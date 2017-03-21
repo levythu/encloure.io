b3e152694961f188b06bcae529eee8a8f5284fb7
Levy <zly.george@163.com>
2017-03-20 23:00:20 -0400

    change some utils to duo version

--- utils/lock.js
+++ utils/lock.js
@@ -4,68 +4,39 @@
 // Author: Levy (levythu)
 // Date: 2016/04/02
 
-var Queue=require("./queue");
+(function() {
+    var Queue=require("./queue");
 
-function Lock(maxHolder)
-{
-    if (maxHolder==null)
-        this.rest=1;
-    else
-        this.rest=maxHolder;
-    this.waitingQueue=new Queue();
-}
-Lock.prototype.Lock=function(callback)
-{
-    if (this.rest==0)
+    function Lock(maxHolder)
     {
-        // No more lock to use, stuck it.
-        this.waitingQueue.EnQueue(callback);
-        return;
+        if (maxHolder==null)
+            this.rest=1;
+        else
+            this.rest=maxHolder;
+        this.waitingQueue=new Queue();
     }
-    this.rest--;
-    callback();
-}
-Lock.prototype.Unlock=function()
-{
-    if (this.waitingQueue.len>0)
+    Lock.prototype.Lock=function(callback)
     {
-        process.nextTick(this.waitingQueue.DeQueue());
-        return;
+        if (this.rest==0)
+        {
+            // No more lock to use, stuck it.
+            this.waitingQueue.EnQueue(callback);
+            return;
+        }
+        this.rest--;
+        callback();
     }
-    this.rest++;
-}
-module.exports=Lock;
+    Lock.prototype.Unlock=function()
+    {
+        if (this.waitingQueue.len>0)
+        {
+            process.nextTick(this.waitingQueue.DeQueue());
+            return;
+        }
+        this.rest++;
+    }
 
-(function()
-{
-    var lk=new Lock(2);
-    lk.Lock(function()
-    {
-        console.log(1);
-        setTimeout(function()
-        {
-            lk.Unlock();
-        }, 3000);
-    });
-    lk.Lock(function()
-    {
-        console.log(2);
-        setTimeout(function()
-        {
-            lk.Unlock();
-        }, 10000);
-    });
-    lk.Lock(function()
-    {
-        console.log(3);
-        setTimeout(function()
-        {
-            lk.Unlock();
-        }, 500);
-    });
-    lk.Lock(function()
-    {
-        console.log(4);
-        lk.Unlock();
-    });
-});
+
+    module.exports=Lock;
+
+})();
