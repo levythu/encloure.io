37580d7c0bf614b5633240eb86fa9eb508af1d77
Levy <zly.george@163.com>
2017-03-26 16:55:18 -0400

    communication module rebuild

--- public/js/playground.comm.js
+++ public/js/playground.comm.js
@@ -6,8 +6,42 @@ var N={};
 //      - gm-init: The game is inited and all the parameters are stored in globalConf;
 
 $(function(){
-    $.get("/gm/getserver", function(data) {
-        console.log(data);
+    var params={};
+    function parseHash() {
+        params={}
+        var hash=window.location.hash.substring(1);
+        var params=hash.split('&');
+        for (var i=0; i<params.length; i++) {
+            var kv=params[i].split('=');
+            var k=decodeURIComponent(pair[0]);
+            var v=decodeURIComponent(pair[1]);
+            params[k]=v;
+        }
+    }
+    function generateHash() {
+        var result="";
+        var theFirst=true;
+        for (var k in params) {
+            if (theFirst) {
+                theFirst=false;
+            } else {
+                result+="&";
+            }
+            result+=encodeURIComponent(k)+"="+encodeURIComponent(params[k]);
+        }
+        return result;
+    }
+    function findOneRoom() {
+        $.get("/gm/getserver", function(data) {
+            params.endpoint=data;
+            window.location.hash=generateHash();
+            connectTo(data);
+        }).fail(function() {
+            alert("Fail to get a game.");
+        });
+    }
+    function connectTo(data) {
+        console.log("Connecting:" + data);
         var connection = new WebSocket(data);
         var hasInitiated=false;
 
@@ -33,8 +67,16 @@ $(function(){
                 return;
             }
             if (!hasInitiated) {
-                hasInitiated=true;
                 globalConf=res;
+                if (typeof(globalConf.profile._fail)=="string") {
+                    // TODO in speficy-room mode it may be a prompt, instead of searching another room
+                    connection.close();
+                    console("Encounter with fail: "+globalConf.profile._fail);
+                    setTimeout(findOneRoom, 500);
+                    return;
+                }
+
+                hasInitiated=true;
                 $("body").trigger("gm-init");
             } else {
                 $("body").trigger("gm-msg", [res]);
@@ -52,7 +94,9 @@ $(function(){
                 console.error(e);
             }
         }
-    }).fail(function() {
-        alert("Fail to get a game.");
-    });
+    }
+
+    if (params.endpoint==null) {
+        findOneRoom();
+    }
 });
