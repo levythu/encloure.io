1a3b196e31ba02d7da992627101c59f78437b63d
Rex <zcnrex@gmail.com>
2017-03-26 20:34:35 +0000

    user.js redirect quickgame to playground with name parameter ;

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
-
 
 var request=require("request");
 
 var conf=require("../settings");
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//===============ROUTES=================
-//displays our homepage
 router.post('/quickgame', function(req, res){
   console.log(req.body.name);
-  res.render('playground', {name: req.body.name})
-  // res.sendFile("playground.html",{
-  //       root: './public/',
-  //       name: req.body.name
-  //   }, function(err) {
-  //       if (err) {
-  //           console.log(err);
-  //       }
-  //   });
+  res.redirect('playground#name='+req.body.name);
+});
+
+router.get('/playground', function(req, res){
+  res.sendFile('playground.html', {
+        root: './public/'
+    }, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
 });
 exports.r = router;
