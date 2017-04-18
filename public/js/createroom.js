aec4762e7dea4cd753faaf74f611b62ecf49a841
Rex <zcnrex@gmail.com>
2017-04-18 22:17:59 +0000

    support thumbnail

--- public/js/createroom.js
+++ public/js/createroom.js
@@ -2,7 +2,7 @@ function loadImage() {
   var map = document.getElementById("mapselect").value;
   // document.getElementById("demo").innerHTML = "You selected: " + x;
   map = map.toLowerCase().replace(" ", "_");
-  var src = "../maps/" + map + ".jpg";
+  var src = "../img/snapshot/" + map + ".png";
   $("#thumbnail").attr("src",src);
 }
 window.onload = function() {
